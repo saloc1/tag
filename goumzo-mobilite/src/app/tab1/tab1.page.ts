28958104ec3e85ea5bbb0214a56b93c63ee74d1f
Quentin Lemaire <quentin.lemaire@ubilink.fr>
2023-02-06 10:03:00 +0100

    recherche acceuil

--- goumzo-mobilite/src/app/tab1/tab1.page.ts
+++ goumzo-mobilite/src/app/tab1/tab1.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+    constructor() {}
+  public data = ['Amsterdam', 'Buenos Aires', 'Cairo', 'Geneva', 'Hong Kong', 'Istanbul', 'London', 'Madrid', 'New York', 'Panama City'];
+  public results = [...this.data];
 
-  constructor() {}
+  handleChange(event:any) {
+    if (event.target.value != "") {
+      const query = event.target.value.toLowerCase();
+      this.results = this.data.filter(d => d.toLowerCase().indexOf(query) > -1);
+    } else {
+      this.results = [];
+    }
+  }
+
 
 }
