59debc9a3d4d84670359552ab250b7973f85cc4b
Quentin Lemaire <quentin.lemaire@ubilink.fr>
2023-02-09 16:53:19 +0100

    AHHHHHHHHHH

--- goumzo-mobilite/src/app/tab1/tab1.page.ts
+++ goumzo-mobilite/src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
+import { OnChanges, Input } from '@angular/core';
+
 interface ApiResponse {
   type: string;
   features: Array<{
@@ -30,10 +32,12 @@ export class Tab1Page {
       console.error(error);
       this.favorites = [];
     }
-
   }
+
+  public autoFilled: boolean = true;
   public results: string[] = [];
   public StartResults: string[] = [];
+  public EndResults: string[] = [];
   public favorites: any[] = [];
   public selectedFavorites: string[] = [];
   public selectedResult: string = '';
@@ -59,6 +63,7 @@ export class Tab1Page {
       this.showSearchList = true;
       this.showEndList = true;
     }
+    this.autoFilled = false;
   }
 
   handleChangeStart(event: any) {
@@ -75,6 +80,22 @@ export class Tab1Page {
     } else {
       this.StartResults = [];
       this.showStartList = true;
+    }
+  }
+
+  handleChangeEnd(event: any) {
+    if (event.target.value !== '') {
+      const query = encodeURIComponent(event.target.value);
+      const apiUrl = `https://data.mobilites-m.fr/api/findType/json?types=arret&query=${query}`;
+
+      this.http.get<ApiResponse>(apiUrl).subscribe(data => {
+        this.EndResults = Array.from(new Set(data.features.map(feature => feature.properties.LIBELLE)));
+        data.features.forEach(feature => {
+          this.coordinates[feature.properties.LIBELLE] = feature.geometry.coordinates;
+        });
+      });
+    } else {
+      this.EndResults = [];
       this.showEndList = true;
     }
   }
@@ -90,7 +111,6 @@ export class Tab1Page {
     this.showStartList = false;
     this.showEndList = false;
   }
-
 
   debug() {
     let startCoordinates = null;
